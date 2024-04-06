53b20fc73fe5d050ee3c5c00585870df57358015
Emnetmamo <131816255+Emnetmamo@users.noreply.github.com>
2024-04-06 21:31:35 +0300

    Update Newsss.js

--- src/pages/Newsss.js
+++ src/pages/Newsss.js
@@ -53,7 +53,7 @@ const News = () => {
               
               <img className='card-img-top' src={`${newsItem.imagePath.replace(/\//g, '\\')}`} alt={newsItem.title} />               
               <div className="card-body text-center">
-                <h6 className="mb-0" style={{color: '#11676d', fontSize: '20px'}}
+                <h6 className="mb-0" style={{color: '#000', fontSize: '20px'}}
                 >Author:  {newsItem.author}</h6>
                 <div className='d-flex'>     
                           
